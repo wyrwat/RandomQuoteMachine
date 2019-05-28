a5ec4cb35ef9986d3f510910ae30e0f1992aebdc
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-28 13:51:16 +0200

    add cors

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    fetch(quoteUrl, { cache: "no-store" })
+    fetch(prefix + quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
